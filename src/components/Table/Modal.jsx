ffef9b67941654dbcb27ae7d0d66f9783af8e2bd
Alexis Moulin <alexis.moulin@icloud.com>
2024-09-11 16:33:32 +0200

    modal amélioré

--- src/components/Table/Modal.jsx
+++ src/components/Table/Modal.jsx
@@ -21,11 +21,24 @@ export default function Modal({open, close, item, headers}) {
 
     return createPortal(
         <dialog ref={dialog}>
-            <h3>Structure Analyzed: {structureName}</h3>
+            <div id="main">
+            <section>
+            <h2>Structure Analyzed: {structureName}</h2>
+            </section>
+            <section>
+            <h3>Key Metrics</h3>
             <ul>
                 {zippedValues.map((el, index) => <li key={index}>{el.join(': ')}</li>)}
             </ul>
+            </section>
+            <section>
+                <h3>Analysis</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+            </section>
+            <section>
             <button onClick={close}>Close</button>
+            </section>
+            </div>
         </dialog>,
         document.getElementById('modal')
     )
