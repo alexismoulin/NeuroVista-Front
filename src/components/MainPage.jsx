2e6f6442eea2604beb2c51d3aa9b23a3767c545b
Alexis Moulin <alexis.moulin@icloud.com>
2024-09-14 15:32:12 +0200

    minor improvements for the prompt and response

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,6 +1,8 @@
 import Intro from "./Intro/Intro.jsx";
 import NavBar from "./NavBar/NavBar.jsx";
 import Table from "./Table/Table.jsx";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faRobot} from "@fortawesome/free-solid-svg-icons";
 
 export default function MainPage({type, handleDefaultType, selectedData, handleSelectedData, setPage, setSelectedItem}) {
     return (
@@ -8,8 +10,13 @@ export default function MainPage({type, handleDefaultType, selectedData, handleS
             <Intro enabled={type} handleType={handleDefaultType} />
             <NavBar selectedType={type} selectedData={selectedData} setSelectedData={handleSelectedData} />
             <div id="main">
-                <h3>{selectedData.title}</h3>
-                <Table {...selectedData} setPage={setPage} setSelectedItem={setSelectedItem} />
+                <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', width: '100%' }}>
+                    <h3 style={{ margin: 0 }}>{selectedData.title}</h3>
+                    <button className="top" onClick={() => {}}>
+                        <FontAwesomeIcon icon={faRobot} size="xl"/>
+                    </button>
+                </div>
+                <Table {...selectedData} setPage={setPage} setSelectedItem={setSelectedItem}/>
             </div>
         </>
     )
