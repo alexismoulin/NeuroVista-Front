31791e13cb4cdc10adb36a0eae50da4e34006955
alexis.moulin <alexis.moulin@icloud.com>
2024-09-17 21:22:24 +0200

    ajout du cerebellum + qq corrections de bugs

--- src/data/data.js
+++ src/data/data.js
@@ -57,7 +57,13 @@ const hypothalamus = {
     headers: ["Structure", "LHS Volume (mm3)", "RHS Volume (mm3)", "Analysis"]
 }
 
+const cerebellum = {
+    data: subcortical.cerebellum,
+    title: "Cerebellum",
+    headers: ["Structure", "LHS Volume (mm3)", "RHS Volume (mm3)", "Analysis"]
+}
+
 export const data = {
     aseg: aseg, brain: brain, whiteMatter: whiteMatter, lhsParcellation: lhsParcellation, rhsParcellation: rhsParcellation,
-    hippocampus: hippocampus, thalamus: thalamus, amygdala: amygdala, brainStem: brainStem, hypothalamus: hypothalamus
+    hippocampus: hippocampus, thalamus: thalamus, amygdala: amygdala, brainStem: brainStem, hypothalamus: hypothalamus, cerebellum:cerebellum
 }
