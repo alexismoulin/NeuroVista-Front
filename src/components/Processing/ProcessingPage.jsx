888fd189e9eeb8695dcf2a2da1c4f3080767c62e
alexis <alexis.moulin@icloud.com>
2024-11-20 10:31:14 +0100

    layout processing page maj

--- src/components/Processing/ProcessingPage.jsx
+++ src/components/Processing/ProcessingPage.jsx
@@ -3,9 +3,19 @@ import GridLoader from "react-spinners/GridLoader";
 import LoadingStep from "./LoadingStep.jsx";
 import CompletedStep from "./CompletedStep.jsx";
 import Copyright from "../Reusable/Copyright.jsx";
+import PrimaryButton from "../Reusable/PrimaryButton.jsx";
 
 export default function ProcessingPage({ setPage }) {
-    const [response, setResponse] = useState({dicom: false, nifti: false, recon: false, subs: false, json: false});
+
+    const steps = [
+        { key: "dicom", completedText: "Dicom upload Completed", loadingText: "Dicom upload in Progress" },
+        { key: "nifti", completedText: "Nifti creation completed", loadingText: "Nifti creation in Progress" },
+        { key: "recon", completedText: "Brain reconstruction completed", loadingText: "Brain reconstruction in Progress (very long - can take up to 16h)" },
+        { key: "subs", completedText: "Extra subcortical segmentations completed", loadingText: "Extra subcortical segmentations in Progress (takes up to 1h)" },
+        { key: "json", completedText: "Json files created", loadingText: "Json files in Progress" },
+    ];
+
+    const [response, setResponse] = useState({dicom: true, nifti: false, recon: false, subs: false, json: false});
     const [loading, setLoading] = useState(true);
 
     useEffect(() => {
@@ -34,51 +44,21 @@ export default function ProcessingPage({ setPage }) {
                     </div>
                 </div>
                 <div>
-                    <ol className="list-decimal p-4 text-slatey font-merriweather border-b-2">
-                        <li>
-                            {
-                                response.dicom ?
-                                    <CompletedStep stepText="Dicom upload Completed"/> :
-                                    <LoadingStep stepText="Dicom upload in Progress"/>
-                            }
-                        </li>
-                        <li>
-                            {
-                                response.nifti ?
-                                    <CompletedStep stepText="Nifti creation completed"/> :
-                                    <LoadingStep stepText="Nifti creation in Progress"/>
-                            }
-                        </li>
-                        <li>
-                            {
-                                response.recon ?
-                                    <CompletedStep stepText="Brain reconstruction completed"/> :
-                                    <LoadingStep
-                                        stepText="Brain reconstruction in Progress (very long - can take up to 16h)"/>
-                            }
-                        </li>
-                        <li>
-                            {
-                                response.subs ?
-                                    <CompletedStep stepText="Extra subcortical segmentations completed"/> :
-                                    <LoadingStep
-                                        stepText="Extra subcortical segmentations in Progress (takes up to 1h)"/>
-                            }
-                        </li>
-                        <li>
-                            {
-                                response.json ?
-                                    <CompletedStep stepText="Json files created"/> :
-                                    <LoadingStep
-                                        stepText="Json files in Progress"/>
-                            }
-                        </li>
+                    <ol className="list-decimal text-slatey font-merriweather border-b-2 my-4">
+                        {steps.map(({ key, completedText, loadingText }) => (
+                            <li key={key} className="my-4 mx-8">
+                                {response[key] ? (
+                                <CompletedStep stepText={completedText} />
+                                ) : (
+                                <LoadingStep stepText={loadingText} />
+                                )}
+                            </li>
+                        ))}
                     </ol>
                 </div>
-                <button onClick={() => {
-                    setPage("main")
-                }}>Main Page
-                </button>
+                <div className="flex justify-center p-6">
+                    <PrimaryButton onClick={() => {setPage("main")}} /*disabled={!response.json}*/>Proceed</PrimaryButton>
+                </div>
             </section>
             <Copyright/>
         </div>
